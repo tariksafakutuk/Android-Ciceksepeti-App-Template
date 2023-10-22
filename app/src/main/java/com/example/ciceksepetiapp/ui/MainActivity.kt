22d423ba816d967da62ff806b19238e5dccff5c4
Tarik Safa Kutuk <tariksafakutuk1905@gmail.com>
2023-10-22 23:33:02 +0300

    :tada: Initial commit

---
+++ app/src/main/java/com/example/ciceksepetiapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ciceksepetiapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ciceksepetiapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
